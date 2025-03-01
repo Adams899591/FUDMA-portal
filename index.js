34bffd83c6dde5f4f7e9c97ebc81b5d141be79b7
Adams899591 <usmanadams551@gmail.com>
2025-03-01 15:13:41 -0800

    gooooooooooo

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ function CheckTnternetQuality() {
       console.log(`speed: ${downlink }msps,  Latency${rtt}ms `);
       
       
-      if (downlink < 0.5 || rtt > 500) {
+      if (downlink < 0.2 || rtt > 2000) {
             window.location.href = "bad Network.html"
             
       }
